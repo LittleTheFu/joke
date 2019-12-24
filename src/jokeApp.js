36f7aeac57a01531586c9cd04df28b4e18a6a0c6
fuyuxiang <lttlstpd@live.com>
2019-12-24 20:51:02 +0800

    add JokeApp file

---
+++ src/jokeApp.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class JokeApp extends Component {
+    render() {
+        return (
+            <div>Joke</div>
+        )
+    }
+}
+
+export default JokeApp;
