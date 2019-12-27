d1e07380a0c6baba8f3f5ec258d4ddd5c3063e80
fuyuxiang <lttlstpd@live.com>
2019-12-27 11:32:35 +0800

    use fetch instead of request

--- src/jokeApp.js
+++ src/jokeApp.js
@@ -12,28 +12,28 @@ class JokeApp extends Component {
       }
 
       getJoke = () => {
-        let request = require("request");
-
-        let options = {
+        let fetchData = {
             method: 'GET',
-            url: 'https://joke3.p.rapidapi.com/v1/joke',
             headers: {
                 'x-rapidapi-host': 'joke3.p.rapidapi.com',
                 'x-rapidapi-key': 'fc5476beb4mshc57aa5e3ed24365p114d83jsn1e6a83699ef6'
-            }
-        };
+            
+        }};
 
-        request(options, (error, response, body) => {
-            if (error) throw new Error(error);
-
-            console.log(body);
-            let data = JSON.parse(body);
-            this.setState ({
-                jokeItem: data.content,
-                upvotes: data.upvotes,
-                downvotes: data.downvotes
+        fetch('https://joke3.p.rapidapi.com/v1/joke', fetchData)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                this.setState ({
+                    jokeItem: data.content,
+                    upvotes: data.upvotes,
+                    downvotes: data.downvotes
+                });
+            })
+            .catch((err) => {
+                console.log(err);
             });
-        });
       }
 
     componentDidMount() {
