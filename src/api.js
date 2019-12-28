d9ae1dfe97c25abaf680bb0875816d0e03072913
fuyuxiang <lttlstpd@live.com>
2019-12-28 17:53:33 +0800

    refactor rawFetchJokeItem

--- src/api.js
+++ src/api.js
@@ -1,22 +1,15 @@
-const rawFetchJokeItem = () => {
+const rawFetchJokeItem = ( {url ,fetchData} ) => {
     return new Promise(function(resolve, reject) {
-        let fetchData = {
-            method: 'GET',
-            headers: {
-                'x-rapidapi-host': 'joke3.p.rapidapi.com',
-                'x-rapidapi-key': 'fc5476beb4mshc57aa5e3ed24365p114d83jsn1e6a83699ef6'        
-            }};
-        
-            fetch('https://joke3.p.rapidapi.com/v1/joke', fetchData)
-                .then((response) => {
-                    return response.json();
-                })
-                .then((data) => {
-                    resolve(data);
-                })
-                .catch((err) => {
-                    reject(err);
-                });
+        fetch(url, fetchData)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            resolve(data);
+        })
+        .catch((err) => {
+            reject(err);
+        });
     });
 };
 
--- src/apiActions.js
+++ src/apiActions.js
@@ -2,7 +2,16 @@ import rawFetchJokeItem from './api';
 
 export const fetchJokeItem = () => {
     return dispatch => {
-        rawFetchJokeItem()
+        rawFetchJokeItem({
+            url: 'https://joke3.p.rapidapi.com/v1/joke',
+            fetchData: {
+                    method: 'GET',
+                    headers: {
+                        'x-rapidapi-host': 'joke3.p.rapidapi.com',
+                        'x-rapidapi-key': 'fc5476beb4mshc57aa5e3ed24365p114d83jsn1e6a83699ef6'        
+                    },
+            }
+        })
         .then((data) => {
             dispatch({
                 type: 'FETCH_JOKE_SUCCESS',
