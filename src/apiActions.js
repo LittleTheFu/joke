f42395e282c8e41bc13bcf73f91170c636b1256a
fuyuxiang <lttlstpd@live.com>
2019-12-29 17:28:07 +0800

    save your history jokes

--- src/apiActions.js
+++ src/apiActions.js
@@ -1,39 +1,50 @@
 import rawFetchJokeItem from './api';
 
-const createFetchData = (id='') => {
+const createFetchData = (id = '') => {
     return {
         url: 'https://joke3.p.rapidapi.com/v1/joke/' + id,
         fetchData: {
-                method: 'GET',
-                headers: {
-                    'x-rapidapi-host': 'joke3.p.rapidapi.com',
-                    'x-rapidapi-key': 'fc5476beb4mshc57aa5e3ed24365p114d83jsn1e6a83699ef6'        
-                },
+            method: 'GET',
+            headers: {
+                'x-rapidapi-host': 'joke3.p.rapidapi.com',
+                'x-rapidapi-key': 'fc5476beb4mshc57aa5e3ed24365p114d83jsn1e6a83699ef6'
+            },
         }
     };
 };
 
-export const fetchJokeItem = (id='') => {
+const createDispatchLocalJokeData = (joke) => {
+    return {
+        type: 'ADD_LOCAL_JOKE',
+        joke: {
+            id: joke.id,
+            jokeItem: joke.content,
+        }
+    };
+};
+
+export const fetchJokeItem = (id = '') => {
     return dispatch => {
         dispatch({
             type: 'BEGIN_LOADING',
         });
         rawFetchJokeItem(createFetchData(id))
-        .then((data) => {
-            dispatch({
-                type: 'FETCH_JOKE_SUCCESS',
-                data,
-             });
-             dispatch({
-                 type: 'END_LOADING',
-             });
-        })
-        .catch((err) => {
-            console.log(err);
-            dispatch({
-                type: 'END_LOADING',
+            .then((data) => {
+                dispatch({
+                    type: 'FETCH_JOKE_SUCCESS',
+                    data,
+                });
+                dispatch(createDispatchLocalJokeData(data));
+                dispatch({
+                    type: 'END_LOADING',
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+                dispatch({
+                    type: 'END_LOADING',
+                });
             });
-        });
     };
 };
 
@@ -59,21 +70,21 @@ export const upvoteJokeItem = (id) => {
             type: 'BEGIN_LOADING',
         });
         rawFetchJokeItem(createUpvoteData(id))
-        .then((data) => {
-            dispatch({
-                type: 'FETCH_JOKE_SUCCESS',
-                data,
-             });
-             dispatch({
-                type: 'END_LOADING',
+            .then((data) => {
+                dispatch({
+                    type: 'FETCH_JOKE_SUCCESS',
+                    data,
+                });
+                dispatch({
+                    type: 'END_LOADING',
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+                dispatch({
+                    type: 'END_LOADING',
+                });
             });
-        })
-        .catch((err) => {
-            console.log(err);
-            dispatch({
-                type: 'END_LOADING',
-            });
-        });
     };
 };
 
@@ -99,20 +110,20 @@ export const downvoteJokeItem = (id) => {
             type: 'BEGIN_LOADING',
         });
         rawFetchJokeItem(createDownvoteData(id))
-        .then((data) => {
-            dispatch({
-                type: 'FETCH_JOKE_SUCCESS',
-                data,
-             });
-             dispatch({
-                type: 'END_LOADING',
+            .then((data) => {
+                dispatch({
+                    type: 'FETCH_JOKE_SUCCESS',
+                    data,
+                });
+                dispatch({
+                    type: 'END_LOADING',
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+                dispatch({
+                    type: 'END_LOADING',
+                });
             });
-        })
-        .catch((err) => {
-            console.log(err);
-            dispatch({
-                type: 'END_LOADING',
-            });
-        });
     };
 };
