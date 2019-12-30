11e7e51cbc724e04742813ec46fa7381aaeecd2e
fuyuxiang <lttlstpd@live.com>
2019-12-30 13:48:53 +0800

    ItemButton can be clicked

--- src/itemButton.js
+++ src/itemButton.js
@@ -2,15 +2,24 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-   border-top: 1px solid blue;
-   text-align:center;
-   font-size:36px;
+    border-top: 1px solid blue;
+    text-align:center;
+    font-size:36px;
+
+    &:hover {
+       background: rgba(213,233,227,1);
+    }
 `;
 
 class ItemButton extends Component {
+
+    hanleClick = () => {
+        console.log('click buttonItem');
+    };
+
     render() {
         return (
-            <Container>
+            <Container onClick={this.hanleClick}>
                 {this.props.data}
             </Container>
         )
