caa30e6452d9521a1f4d95bf527263aa71cef536
fuyuxiang <lttlstpd@live.com>
2019-12-30 10:45:18 +0800

    clean code

--- src/jokeList.js
+++ src/jokeList.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 
 class JokeList extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <div>
                 {this.props.items.map((item, index) => 
-                    (<h1 key={index}>{item.id} ## {item.jokeItem}</h1>)
+                    (<h1 key={index}>{item.jokeItem}</h1>)
                 )}
             </div>
         )
