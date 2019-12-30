86fbcd7ae9b064b6cd28d18d0fff9b30cbedd8ae
fuyuxiang <lttlstpd@live.com>
2019-12-30 19:28:17 +0800

    refactor functions

--- src/apiActions.js
+++ src/apiActions.js
@@ -48,10 +48,10 @@ export const fetchJokeItem = (id = '') => {
     };
 };
 
-const createUpvoteData = (id) => {
-    console.log("https://joke3.p.rapidapi.com/v1/joke/" + id + "/upvote");
+const createVoteData = (id, isUpvote) => {
+    let voteType = isUpvote ? "upvote" : "downvote";
     return {
-        url: "https://joke3.p.rapidapi.com/v1/joke/" + id + "/upvote",
+        url: "https://joke3.p.rapidapi.com/v1/joke/" + id + "/" + voteType,
         fetchData: {
             "method": "POST",
             "headers": {
@@ -62,68 +62,38 @@ const createUpvoteData = (id) => {
             "body": {},
         }
     };
+};
+
+const voteJokeItem = (id, isUpvote, dispatch) => {
+    dispatch({
+        type: 'BEGIN_LOADING',
+    });
+    rawFetchJokeItem(createVoteData(id, isUpvote))
+        .then((data) => {
+            dispatch({
+                type: 'FETCH_JOKE_SUCCESS',
+                data,
+            });
+            dispatch({
+                type: 'END_LOADING',
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            dispatch({
+                type: 'END_LOADING',
+            });
+        });
 };
 
 export const upvoteJokeItem = (id) => {
     return dispatch => {
-        dispatch({
-            type: 'BEGIN_LOADING',
-        });
-        rawFetchJokeItem(createUpvoteData(id))
-            .then((data) => {
-                dispatch({
-                    type: 'FETCH_JOKE_SUCCESS',
-                    data,
-                });
-                dispatch({
-                    type: 'END_LOADING',
-                });
-            })
-            .catch((err) => {
-                console.log(err);
-                dispatch({
-                    type: 'END_LOADING',
-                });
-            });
-    };
-};
-
-const createDownvoteData = (id) => {
-    console.log("https://joke3.p.rapidapi.com/v1/joke/" + id + "/downvote");
-    return {
-        url: "https://joke3.p.rapidapi.com/v1/joke/" + id + "/downvote",
-        fetchData: {
-            "method": "POST",
-            "headers": {
-                "x-rapidapi-host": "joke3.p.rapidapi.com",
-                "x-rapidapi-key": "fc5476beb4mshc57aa5e3ed24365p114d83jsn1e6a83699ef6",
-                "content-type": "application/x-www-form-urlencoded"
-            },
-            "body": {},
-        }
+        voteJokeItem(id, true, dispatch);
     };
 };
 
 export const downvoteJokeItem = (id) => {
     return dispatch => {
-        dispatch({
-            type: 'BEGIN_LOADING',
-        });
-        rawFetchJokeItem(createDownvoteData(id))
-            .then((data) => {
-                dispatch({
-                    type: 'FETCH_JOKE_SUCCESS',
-                    data,
-                });
-                dispatch({
-                    type: 'END_LOADING',
-                });
-            })
-            .catch((err) => {
-                console.log(err);
-                dispatch({
-                    type: 'END_LOADING',
-                });
-            });
+        voteJokeItem(id, false, dispatch);
     };
 };
