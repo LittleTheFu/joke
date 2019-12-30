5c4efd9f865ebb742f6f3b54861ee123b45332fe
fuyuxiang <lttlstpd@live.com>
2019-12-30 20:08:06 +0800

    fix clear joke bug

--- src/localReducer.js
+++ src/localReducer.js
@@ -10,8 +10,12 @@ const initialState = {
  const localReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'ADD_LOCAL_JOKE':
-            if(action.joke.id === state.recentJokes[state.recentJokes.length - 1].id) {
-                return state;
+            let isEmpty = (state.recentJokes.length === 0);
+            if( !isEmpty ) {
+                if(action.joke.id === state.recentJokes[state.recentJokes.length - 1].id)
+                {
+                    return state;
+                }
             }
             let jokes = state.recentJokes;
             if ( jokes.length >= MAX_LOCAL_NUM ) {
